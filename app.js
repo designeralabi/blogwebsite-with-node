459056889b1010889344e53e92adffb69de0dd5b
designeralabi <bdlkrmalabi@gmail.com>
2022-01-02 20:38:56 +0100

    post route setup

--- app.js
+++ app.js
@@ -1,31 +1,50 @@
-//jshint esversion:6
-
 const express = require("express");
 const bodyParser = require("body-parser");
 const ejs = require("ejs");
-
-// const homeStartingContent = ";
-// const aboutContent = ;
-// const contactContent = ;
-
 const app = express();
+
 const port = 3000;
+
 app.set('view engine', 'ejs');
 
 app.use(bodyParser.urlencoded({extended: true}));
 app.use(express.static("public"));
 
+const blogPost = [];
+
+// homepage request
 app.get('/', (req, res)=>{
   res.render('home', {route: 'homepage'});
 });
 
+// aboutpage request
 app.get('/about', (req, res)=>{
   res.render('about', {route: 'about'});
 });
 
+// contact page request
 app.get('/contact', (req, res)=>{
   res.render('contact', {route: 'contact'});
 });
+
+app.get('/post', (req, res)=>{
+  res.render('post');
+
+  const blogHeader = req.body.blogHeader;
+  const contentText = req.body.contentText;
+
+  // const newBlog =  blogHeader + contentText;
+
+  console.log(blogHeader, contentText);
+});
+
+
+app.post('/', (req, res)=>{
+
+  // res.render('post', {blogData: newBlog});
+  res.send("Submitted Successfully");
+});
+
 
 
 
