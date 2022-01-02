4ee4d00aced3e2d72a99f0fc323b724d96adbe35
designeralabi <bdlkrmalabi@gmail.com>
2022-01-02 04:01:54 +0100

    added req route for other pages

--- app.js
+++ app.js
@@ -5,8 +5,8 @@ const bodyParser = require("body-parser");
 const ejs = require("ejs");
 
 // const homeStartingContent = ";
-const aboutContent = "Hac habitasse platea dictumst vestibulum rhoncus est pellentesque. Dictumst vestibulum rhoncus est pellentesque elit ullamcorper. Non diam phasellus vestibulum lorem sed. Platea dictumst quisque sagittis purus sit. Egestas sed sed risus pretium quam vulputate dignissim suspendisse. Mauris in aliquam sem fringilla. Semper risus in hendrerit gravida rutrum quisque non tellus orci. Amet massa vitae tortor condimentum lacinia quis vel eros. Enim ut tellus elementum sagittis vitae. Mauris ultrices eros in cursus turpis massa tincidunt dui.";
-const contactContent = "Scelerisque eleifend donec pretium vulputate sapien. Rhoncus urna neque viverra justo nec ultrices. Arcu dui vivamus arcu felis bibendum. Consectetur adipiscing elit duis tristique. Risus viverra adipiscing at in tellus integer feugiat. Sapien nec sagittis aliquam malesuada bibendum arcu vitae. Consequat interdum varius sit amet mattis. Iaculis nunc sed augue lacus. Interdum posuere lorem ipsum dolor sit amet consectetur adipiscing elit. Pulvinar elementum integer enim neque. Ultrices gravida dictum fusce ut placerat orci nulla. Mauris in aliquam sem fringilla ut morbi tincidunt. Tortor posuere ac ut consequat semper viverra nam libero.";
+// const aboutContent = ;
+// const contactContent = ;
 
 const app = express();
 const port = 3000;
@@ -17,6 +17,14 @@ app.use(express.static("public"));
 
 app.get('/', (req, res)=>{
   res.render('home', {route: 'homepage'});
+});
+
+app.get('/about', (req, res)=>{
+  res.render('about', {route: 'about'});
+});
+
+app.get('/contact', (req, res)=>{
+  res.render('contact', {route: 'contact'});
 });
 
 
